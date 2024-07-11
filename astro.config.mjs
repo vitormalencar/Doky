5703e6764652111b0f7224bdb8863bac64318633
Vitor Alencar <vitor.maia.alencar@gmail.com>
2024-07-11 17:17:36 +0200

    chore: adding config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,18 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'Doky',
-    social: {
-      github: 'https://github.com/maykbrito/doky'
-    }
-    // sidebar: [
-    // 	{
-    // 		label: 'Guides',
-    // 		items: [
-    // 			// Each item here is one entry in the navigation menu.
-    // 			{ label: 'Example Guide', slug: 'guides/example' },
-    // 		],
-    // 	},
-    // 	{
-    // 		label: 'Reference',
-    // 		autogenerate: { directory: 'reference' },
-    // 	},
-    // ],
-  })],
+  site: "https://maykbrito.github.io/doky",
+  integrations: [
+    starlight({
+      title: "Doky",
+      social: {
+        github: "https://github.com/maykbrito/doky",
+      },
+    }),
+  ],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
